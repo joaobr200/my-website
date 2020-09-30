97be42d58f0ab8c78447df8883937465d6a6caa3
joaobr200 <joaoarantesjob@gmail.com>
2020-09-30 12:19:53 -0300

    change the section portifolio to projects

---
+++ src/components/projects/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import { Container } from "./styles";
+
+const Projects: React.FC = () => {
+  return (
+    <Container>
+      <h1>Projetos</h1>
+      <p>Carregando...</p>
+    </Container>
+  );
+};
+
+export default Projects;
