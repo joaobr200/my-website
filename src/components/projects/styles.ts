00182de4d83755155a4138231dd8d04ef7e8bfc6
joaobr200 <joaoarantesjob@gmail.com>
2020-10-07 10:53:29 -0300

    add projects in my projects

--- src/components/projects/styles.ts
+++ src/components/projects/styles.ts
@@ -1,8 +1,7 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Container = styled.section`
   width: 100%;
-  height: 100vh;
   padding: 8px;
 
   display: flex;
@@ -11,7 +10,7 @@ export const Container = styled.section`
 
   h1 {
     font-size: 4.8rem;
-    padding-bottom: 180px;
+    padding-bottom: 100px;
   }
 
   h1::after {
@@ -26,4 +25,60 @@ export const Container = styled.section`
   p {
     font-size: 3.2rem;
   }
+`;
+
+export const ProjectWrapper = styled.div`
+      width:100%;
+
+      display:flex;
+      flex-wrap:wrap;
+`;
+
+
+export const ProjectArticle = styled.article`
+    position: relative;
+
+    width:250px;
+    height:230px;
+    margin: 15px;
+    
+    border-radius:8px;
+
+    background:${({theme}) => theme.colors.gray};
+    box-shadow:0 0 5px 2.5 rgba(0,0,0,0.8);
+    
+    
+    cursor:pointer;
+    transition: all .2s ease;
+
+    &:hover {
+        opacity:0.9;
+    }
+
+    div.body {
+        padding: 8px;
+        p {
+            font-size: 1.2rem;        
+        }
+
+        p.project {
+            font-weight: 700;
+        }
+        p.description {
+          font-size:1rem;
+        }
+    }
+`;
+
+export const ProjectIcon = styled.div`
+    position: absolute;
+    top:150px;
+    right:0;
+    
+`;
+
+
+export const Image = styled.img`
+      width:250px;
+      height:150px;    
 `;
