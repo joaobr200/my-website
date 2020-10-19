9565dc40a9bd09590233d94a22d4a0c194b98e57
Joaobr200 <joaoarantesjob@gmail.com>
2020-10-19 16:36:14 -0300

    add new project

--- src/components/projects/projects.ts
+++ src/components/projects/projects.ts
@@ -2,26 +2,25 @@ export default [
   {
     name: "Weather Now",
     description: "Applicativo para buscar o clima de sua cidade.",
-    url:
-      "https://github.com/joaobr200/WeatherNow",
-    imageUri:
-      "/projects/weather-now.png",
+    url: "https://github.com/joaobr200/WeatherNow",
+    imageUri: "/projects/weather-now.png",
   },
   {
     name: "AppBiblia",
     description: "App de busca de livros da bíblia através de uma API.",
-    url:
-      "https://github.com/joaobr200/AppBiblia",
-    imageUri:
-      "/projects/app-biblia.png",
+    url: "https://joaobr200.github.io/AppBiblia/",
+    imageUri: "/projects/app-biblia.png",
   },
   {
     name: "REST Web API",
     description: "API com autenticação de usuários.",
-    url:
-      "https://github.com/joaobr200/express-api",
-    imageUri:
-      "/projects/api.png",
+    url: "https://github.com/joaobr200/express-api",
+    imageUri: "/projects/api.png",
   },
-  
+  {
+    name: "Foodima",
+    description: "Site de receitas culinárias fictício",
+    url: "https://github.com/joaobr200/Foodima",
+    imageUri: "/projects/foodima.png",
+  },
 ];
--- src/components/projects/styles.ts
+++ src/components/projects/styles.ts
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const Container = styled.section`
   width: 100%;
-  padding: 8px;
+  padding: 8px 0 50px 0;
 
   display: flex;
   align-items: center;
@@ -28,57 +28,54 @@ export const Container = styled.section`
 `;
 
 export const ProjectWrapper = styled.div`
-      width:100%;
+  width: 100%;
 
-      display:flex;
-      flex-wrap:wrap;
+  display: flex;
+  flex-wrap: wrap;
 `;
 
-
 export const ProjectArticle = styled.article`
-    position: relative;
+  position: relative;
 
-    width:250px;
-    height:230px;
-    margin: 15px;
-    
-    border-radius:8px;
+  width: 250px;
+  height: 230px;
+  margin: 15px;
 
-    background:${({theme}) => theme.colors.gray};
-    box-shadow:0 0 5px 2.5 rgba(0,0,0,0.8);
-    
-    
-    cursor:pointer;
-    transition: all .2s ease;
+  border-radius: 8px;
 
-    &:hover {
-        opacity:0.9;
+  background: ${({ theme }) => theme.colors.gray};
+  box-shadow: 0 0 5px 2.5 rgba(0, 0, 0, 0.8);
+
+  cursor: pointer;
+  transition: all 0.2s ease;
+
+  &:hover {
+    opacity: 0.9;
+  }
+
+  div.body {
+    padding: 8px;
+    p {
+      font-size: 1.2rem;
     }
 
-    div.body {
-        padding: 8px;
-        p {
-            font-size: 1.2rem;        
-        }
-
-        p.project {
-            font-weight: 700;
-        }
-        p.description {
-          font-size:1rem;
-        }
+    p.project {
+      font-weight: 700;
     }
+    p.description {
+      font-size: 1rem;
+    }
+  }
 `;
 
 export const ProjectIcon = styled.div`
-    position: absolute;
-    top:150px;
-    right:0;
-    
+  position: absolute;
+  top: 150px;
+  right: 0;
 `;
 
-
 export const Image = styled.img`
-      width:250px;
-      height:150px;    
+  width: 250px;
+  height: 150px;
+  object-fit: cover;
 `;
